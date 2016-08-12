0f264a44fc910d773e142c01afc17fecf86ec83c
yizhixiaobai <1042897963@qq.com>
2016-08-12 15:42:52 +0800

    https://leetcode.com/problems/move-zeroes/

--- LeetCode-283.cpp
+++ LeetCode-283.cpp
@@ -1,3 +1,4 @@
+//O(n^2)解法，类似插入排序
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
@@ -9,5 +10,18 @@ public:
             }
             nums[j+1] = key;
         }
+    }
+};
+//O(n)解法
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0,j=0;
+        for(; i < nums.size(); i++){
+            if(nums[i])
+                nums[j++] = nums[i];
+        }
+        for(;j < nums.size(); j++)
+            nums[j] = 0;
     }
 };
