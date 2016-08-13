37d9d33560bf06f3071748a3ba74ea4d5bc246a9
yizhixiaobai <1042897963@qq.com>
2016-08-13 17:35:01 +0800

    https://leetcode.com/problems/plus-one/

--- LeetCode-66.cpp
+++ LeetCode-66.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int carry = 1;
         for(int i = digits.size()-1; i >= 0; i--){
-            digits[i] += carry;
+            digits[i]++;
             if(digits[i] <= 9)
                 return digits;
             digits[i] = 0;
-            carry = 1;
         }
         digits.insert(digits.begin(), 1);
         return digits;
